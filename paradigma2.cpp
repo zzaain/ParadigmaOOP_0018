fdfba359c08bb9a3c364649f1406519d22c99710
zzaain <sheriellaghzain@gmail.com>
2026-04-20 11:22:38 +0700

    menambahkan method printData

--- paradigma2.cpp
+++ paradigma2.cpp
@@ -7,5 +7,13 @@ class Barang{
     int jumlah;
     string kategori;
     string tanggalProduksi;
-}
 
+     void printData (){
+
+        cout << " Nama barang  : " << nama << endl;
+        cout << " Jumlah : " << jumlah << endl;
+        cout << " Kategori : " << kategori << endl;
+        cout << " tanggalProduksi : " << tanggalProduksi << endl;
+        
+    }
+}; // batas class
